42d37d9ef5cc0275bf14aa32b404e302e245b08a
Ryan Kraus <rmkraus@gmail.com>
2019-05-07 00:29:22 -0400

    Added host TMUX support to linux launcher.

--- bin/linux.sh
+++ bin/linux.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+_=$(which tmux)
+TMUX_FOUND=$?
+_=$(env | grep -i tmux)
+INTMUX=$?
 
-clear; /usr/local/bin/docker exec -it workspace /bin/bash
+if [[ $TMUX_FOUND -eq 0 && $INTMUX -eq 1 ]]; then
+    tmux new /usr/local/bin/docker exec -it workspace /bin/bash
+else
+    /usr/local/bin/docker exec -it workspace /bin/bash
+fi
